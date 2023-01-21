394f5e6a0cbccc03610ff15e21c6c5585894909f
Deux-Ailes <Grosyo49@>
2023-01-21 12:42:31 +0100

    Initial Commit

---
+++ MindMuscle/MindMuscleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMuscleApp.swift
+//  MindMuscle
+//
+//  Created by Elliot Clerice on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MindMuscleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
